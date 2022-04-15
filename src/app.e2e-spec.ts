dcf7f640677eadf1099aafbbd52db21e810271c2
Adriane Ribeiro <adrianeribeiro94@gmail.com>
2022-04-15 15:37:14 -0300

    validando inserçao de dados no banco

--- src/app.e2e-spec.ts
+++ src/app.e2e-spec.ts
@@ -1,11 +1,22 @@
+import { prisma } from './prisma'
 import request from 'supertest'
 import { app } from './app'
+import crypto from 'node:crypto';
 
 test('[e2e] CreateLesson', async () => {
+  const title = `Nova Aula_${crypto.randomUUID()}`
+
   const response = await request(app)
     .post('/lessons')
-    .send({title: 'Nova Aula'})
+    .send({title: title})
+
+  const lessonInDatabase = await prisma.lesson.findFirst({
+    where: {
+      title: title
+    }
+  })
 
   expect(response.status).toBe(201)
   expect(response.body.error).toBeFalsy()
+  expect(lessonInDatabase).toBeTruthy()
 })
